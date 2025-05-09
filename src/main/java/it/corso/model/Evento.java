deeae4c4ceb70835766ca54b8670113052b568d8
Checco <francesco.zaza0510@gmail.com>
2025-05-09 12:42:18 +0200

    modifica sql ed add model evento

---
+++ src/main/java/it/corso/model/Evento.java
@@ -0,0 +1,40 @@
+package it.corso.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "evento")
+public class Evento {
+    
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+    private String foto;
+    private String nome;
+    private LocalDate ricezione;
+    private Integer costo;
+    private String campo;
+    private String descrizione;
+    private Integer partecipanti;
+    
+    @ManyToOne(cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "id_indirizzo",referencedColumnName="id")
+    private Indirizzo indirizzo;
+
+    @ManyToOne(cascade = CascadeType.REFRESH)
+    @JoinColumn(name= "id_sport", referencedColumnName = "id")
+    private Sport sport;
+}
