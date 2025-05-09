9ddaf852c8799c10598ca374e24a796a7c9a1b30
Giuseppe Passaro <giuseppepassaro9@gmail.com>
2025-05-09 12:17:09 +0200

    Admin Entity

--- src/main/java/it/corso/model/Admin.java
+++ src/main/java/it/corso/model/Admin.java
@@ -1,5 +1,25 @@
 package it.corso.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "admin")
 public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String username;
+
+    private String psw;
 
 }
