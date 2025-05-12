39d26040e2c7085be6c5afca6d4e2c462f9c1541
LucaRosan <rosaticv@gmail.com>
2025-05-12 10:22:14 +0200

    Update RiservataController.java

--- src/main/java/it/corso/controller/RiservataController.java
+++ src/main/java/it/corso/controller/RiservataController.java
@@ -3,13 +3,12 @@ package it.corso.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.corso.model.Admin;
 import it.corso.service.AdminService;
 import jakarta.servlet.http.HttpSession;
-
-import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
@@ -32,7 +31,7 @@ public class RiservataController {
 
     @GetMapping("/logout")
     public String logoutCliente(HttpSession session) {
-        session.removeAttribute("cliente");
+        session.removeAttribute("admin");
         return "redirect:/";
     }
     
